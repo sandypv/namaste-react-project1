ef758722427c680c7ed1f9a5819e3110b3a4f3b6
Sandhya <sandhya25rani@gmail.com>
2024-12-10 18:39:27 +0530

    Added header component with navigation links

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+
+const Header = () => {
+  const [btnName, setBtnName] = useState("login");
+
+  const handleLoginBtn = () => {
+    return btnName === "login" ? setBtnName("logout") : setBtnName("login");
+  };
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button className="login-btn" onClick={handleLoginBtn}>
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
