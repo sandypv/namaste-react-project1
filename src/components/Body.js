8f05b65176236d363830047bab030167b5f2b81a
Sandhya <sandhya25rani@gmail.com>
2024-12-10 19:16:25 +0530

    Added a comment on slicing the first 3 cards.

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,8 +16,8 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
     );
     const result = await response.json();
+    //The first 3 cards have config info,so slicing it by 3.
     const restaurantInfo = result?.data?.cards.slice(3);
-    // console.log(restaurantInfo);
     setListOfRestaurants(restaurantInfo);
     setFilteredRestaurants(restaurantInfo);
   };
